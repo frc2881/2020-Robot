10bfa4b5b90ec465127f80b17d2caffbd0c0f61c
Lady Cans <frc2881@gmail.com>
2020-01-31 19:35:41 -0600

    Ready to be tested and picked apart
    STUDENTS: Tian

--- src/main/java/frc/robot/commands/IntakeControlRollers.java
+++ src/main/java/frc/robot/commands/IntakeControlRollers.java
@@ -69,6 +69,9 @@ public class IntakeControlRollers extends Command {
     // Called once after isFinished returns true
     @Override
     protected void end() {
+        Robot.intake.intakeFeeding(0, RollerDirection.INTAKE);
+        Robot.intake.intakeRightLeft(0, RollerDirection.INTAKE);
+        Robot.intake.intakeParallelBand(0, RollerDirection.INTAKE);
     }
 
     // Called when another command which requires one or more of the same
