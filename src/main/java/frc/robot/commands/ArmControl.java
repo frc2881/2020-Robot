f1ee0a91168eb8d6096d07d62b04a5f085ba91fd
Lady Cans <frc2881@gmail.com>
2020-02-15 18:03:55 -0600

    Change direction of joystick Student Samantha

--- src/main/java/frc/robot/commands/ArmControl.java
+++ src/main/java/frc/robot/commands/ArmControl.java
@@ -42,7 +42,7 @@ public class ArmControl extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-        double left = -Robot.oi.getManipulatorLeftY();
+        double left = Robot.oi.getManipulatorLeftY();
         Robot.arm.setArmSpeed(left);
     }
 
