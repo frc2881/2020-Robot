f1a0f8b6ae3127f76f8f32e5295d069999056caa
Lady Cans <frc2881@gmail.com>
2020-02-16 14:57:16 -0600

    ready to test auto alignment code
    STUDENTS: Tian

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,7 +23,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
-import frc.robot.Utils.NavX;
+import frc.robot.utils.NavX;
 
 
 import frc.robot.commands.AutonomousCommand;
