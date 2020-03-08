a1db890d4f5726690c3dcc52823c305975d4acb0
Lady Cans <frc2881@gmail.com>
2020-03-08 17:47:36 -0500

    reverted last change

--- src/main/java/frc/robot/commands/autonomous/autoCommands/Enums/AutoOptions.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.commands.autonomous.autoCommands.Enums;
-
-public enum AutoOptions {
-    CROSS_LINE, SCORE
-}
--- src/main/java/frc/robot/commands/autonomous/autoCommands/Enums/StartingPosition.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.commands.autonomous.autoCommands.Enums;
-
-public enum StartingPosition {
-    PUSH_ALLIANCE_MEMBER, FRONT_ON_TEN_FOOT_ARC
-}
