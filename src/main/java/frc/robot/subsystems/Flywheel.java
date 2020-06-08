4284cecf7557adc8a86c9b356c9562030e677fd4
Lady Cans <frc2881@gmail.com>
2020-06-08 18:38:26 -0500

    added the initial range for the flywheel during shooting
    Emily
    Brian

--- src/main/java/frc/robot/subsystems/Flywheel.java
+++ src/main/java/frc/robot/subsystems/Flywheel.java
@@ -4,7 +4,6 @@ import java.util.function.DoubleSupplier;
 
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
-import com.revrobotics.CANPIDController.AccelStrategy;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -13,22 +12,22 @@ import com.revrobotics.ControlType;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import frc.robot.commands.scoring.flywheel.ControlFlywheel;
-import frc.robot.subsystems.Intake.RollerDirection;
 import frc.robot.utils.frc4048.Logging;
-
 
 public class Flywheel extends Subsystem {
 
-    public enum FlywheelStates{FULL, HALF, STOP}
+    public enum FlywheelStates {
+        FULL, HALF, STOP
+    }
 
     public boolean flywheelReady = false;
 
-    //Good Curve Numbers: P: 0.0001, I:  0.000000126275, D: 0.001
-    public final double kP = 0.0000125;//0.0015; //0.002
-    public final double kI = 0.000000126275; //tuning 0.000001, 0.0000005, 0.00000025085, 0.000000126275,  0.0000000017
-    public final double kD = 0.015;//0.0002; //0.0012 
-    public final double kIz = 0; 
-    public final double kFF = 0; //0.000173; //around 1 over 6000, based off of original P value of testing
+    // Good Curve Numbers: P: 0.0001, I: 0.000000126275, D: 0.001
+    public final double kP = 0.0000125;// 0.0015; //0.002
+    public final double kI = 0.000000126275; // tuning 0.000001, 0.0000005, 0.00000025085, 0.000000126275, 0.0000000017
+    public final double kD = 0.015;// 0.0002; //0.0012
+    public final double kIz = 0;
+    public final double kFF = 0; // 0.000173; //around 1 over 6000, based off of original P value of testing
     public final double kMaxOutput = 1;
     public final double kMinOutput = 0;
     public final double maxRPM = 5700;
@@ -40,6 +39,7 @@ public class Flywheel extends Subsystem {
 
     private boolean flywheelStop = true;
     private boolean flywheelFullSpeed = false;
+    private boolean upTo3450RPM;
 
     public Flywheel() {
 
@@ -70,8 +70,6 @@ public class Flywheel extends Subsystem {
         return flywheelStop;
     }
 
-    
-
     public void setFlywheelSpeedState(final FlywheelStates speed) {
         if (speed == FlywheelStates.STOP) {
             flywheelStop = true;
@@ -99,9 +97,17 @@ public class Flywheel extends Subsystem {
     }
 
     public boolean isFlywheelReady() {
-        //TODO: initial ready range should be 3450, afterwards stay within +or- 200 (stay within PID oscillation)
+
         final boolean readyForHigh = getFlywheelRPM() > 4700 && isFlywheelFullSpeed();
-        final boolean readyForLow = getFlywheelRPM() > 3300 && getFlywheelRPM() < 3700 && !isFlywheelFullSpeed();
+        // Low Speed
+        if (getFlywheelRPM() <= 1000) {
+            upTo3450RPM = false;
+        }
+        if (getFlywheelRPM() >= 3450) {
+            upTo3450RPM = true;
+        }
+        final boolean readyForLow = upTo3450RPM == true && getFlywheelRPM() > 3300 && getFlywheelRPM() < 3700 && !isFlywheelFullSpeed();
+        // TODO: initial ready range should be 3450, afterwards stay within +or- 200 (stay within PID oscillation)
 
         return (readyForHigh || readyForLow) && !flywheelStop;
     }
@@ -134,10 +140,10 @@ public class Flywheel extends Subsystem {
 
     }
 
-    public Logging.LoggingContext loggingContext = new Logging.LoggingContext(Logging.Subsystems.FLYWHEEL){
+    public Logging.LoggingContext loggingContext = new Logging.LoggingContext(Logging.Subsystems.FLYWHEEL) {
 
         @Override
-        protected void addAll(){
+        protected void addAll() {
             add("FLYWHEEL: BusVoltage", flywheel.getBusVoltage());
             add("FLYWHEEL: OutputCurrent", flywheel.getOutputCurrent());
             add("FLYWHEEL: StickyFaults", flywheel.getStickyFaults());
